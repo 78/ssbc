ddd9cb3e8e400decfa88e337e817022f8089e58c
job <job@DQ5520-32.(none)>
2015-05-09 17:30:21 -0700

    migrate to django

---
+++ web/static/js/ssbc.js
@@ -0,0 +1,92 @@
+var PVCC = PVCC || {
+    setCookie: function(cname,cvalue,exsecs)
+    {   
+        var d = new Date();
+        d.setTime(d.getTime()+(exsecs*1000));
+        var expires = "expires="+d.toGMTString();
+        document.cookie = cname + "=" + cvalue + "; " + expires;
+    },  
+    getCookie: function(cname)
+    {   
+        var name = cname + "=";
+        var ca = document.cookie.split(';');
+        for(var i=0; i<ca.length; i++) 
+        {   
+            var c = ca[i].trim();
+            if (c.indexOf(name)==0) return c.substring(name.length,c.length);
+        }   
+        return ""; 
+    },
+    getVar: function(name) {
+        name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+        var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"), results = regex.exec(location.search);
+        return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+    }
+};
+
+$('.x-sform').submit(function(e){
+    e.preventDefault();
+    var kw = $('.x-kw').val();
+    if(!kw){
+        $('.x-kw').focus();
+        return false;
+    }
+    var url = '/list/' + encodeURIComponent(kw) + '/1';
+    window.location = url;
+    return false;
+});
+
+function showAds(){
+    var ua = navigator.userAgent;
+    if(ua.indexOf('iP') > -1 || ua.indexOf('Android') > -1){ //移动端排版
+    }else if(ua.indexOf('Apple1') > -1 && window.location.pathname=='/'){ //苹果
+    }else{
+        //$('.div-ads-front').html('<a href="http://love.gemblepan.com/?from=frontads" target="_blank"><img src="/static/ss_ad.png"></a>');
+        //$('.div-ads').html('<a href="http://love.gemblepan.com/?from=ads" target="_blank"><img src="/static/ss_banner.gif"></a>');
+        if(window.location.href.indexOf('/info/') > -1 || window.location.href.indexOf('/list/') > -1){
+            document.write('<script type="text/javascript" src="http://cip2.czpush.com/promote.php?id=100072"><\/script>');
+        }
+    }
+
+}
+
+$(function(){
+    var $a = $('.good .tips a');
+    if($a.length > 0){
+        $a.attr('href', 'http://love.gemblepan.com/?from=ssbc');
+        $a.attr('target', '_blank');
+    }
+    var ref = document.referrer;
+    if(ref && ref.indexOf('shousibaocai') == -1){
+        PVCC.setCookie('ref', 'somewhere');
+    }else if(!ref){
+        PVCC.setCookie('ref', 'direct');
+    }
+    if(PVCC.getCookie('ref') == 'somewhere'){
+    }
+});
+
+showAds();
+
+
+$(".x-kw").typeahead({
+    onSelect: function(item) {
+        console.log(item);
+    },
+    ajax: {
+        url: "/suggest",
+        timeout: 500,
+        displayField: "suggestion",
+        triggerLength: 1,
+        method: "get",
+        preProcess: function (data) {
+            if (data.error) {
+                // Hide the list, there was some error
+                return false;
+            }
+            // We good!
+            return data.suggest.suggestions;
+        }
+    }
+});
+
---
+++ web/views.py
@@ -0,0 +1,84 @@
+#coding: utf8
+import re
+import datetime
+import sys
+import urllib
+import requests
+from django.http import Http404
+from django.shortcuts import render
+
+API_URL = 'http://127.0.0.1:8000/api/'
+re_punctuations = re.compile(
+    u"。|，|,|！|…|!|《|》|<|>|\"|'|:|：|？|\?|、|\||“|”|‘|’|；|—|（|）|·|\(|\)|　|\.|【|】|『|』|@|&|%|\^|\*|\+|\||<|>|~|`|\[|\]")
+
+# Create your views here.
+def index(request):
+    reclist = ['速度与激情7','王牌特工','战狼','左耳','咱们结婚吧']
+    d = {'reclist': reclist}
+    return render(request, 'index.html', d)
+
+def hash(request, h):
+    qs = {
+        'hashes': h,
+    }
+    url = API_URL + 'json_info?' + urllib.urlencode(qs)
+    r = requests.get(url)
+    try:
+        j = r.json()
+    except:
+        raise Http404(sys.exc_info()[1])
+    d = {'info': j[h]} 
+    d['keywords'] = list(set(re_punctuations.sub(u' ', d['info']['name']).split()))
+    if 'files' in d['info']:
+        d['info']['files'] = [y for y in d['info']['files'] if not y['path'].startswith(u'_')]
+        d['info']['files'].sort(key=lambda x:x['length'], reverse=True)
+    d['magnet_url'] = 'magnet:?' + urllib.urlencode({'xt':'urn:btih:%s'% (d['info']['info_hash']), 'dn':d['info']['name'].encode('utf8')})
+    d['download_url'] = 'http://www.so.com/s?' + urllib.urlencode({'ie':'utf-8', 'src': 'ssbc', 'q': d['info']['name'].encode('utf8')})
+    return render(request, 'info.html', d)
+
+
+def search(request, keyword, p):
+    d = {'keyword': keyword}
+    d['words'] = list(set(re_punctuations.sub(u' ', d['keyword']).split()))
+    try:
+        d['p'] = int(p)
+    except:
+        d['p'] = 1
+    d['ps'] = 10
+    d['offset'] = d['ps']*(d['p']-1)
+    # Fetch list
+    qs = {
+        'keyword': keyword.encode('utf8'),
+        'count': d['ps'],
+        'start': d['offset'],
+    }
+    url = API_URL + 'json_search?' + urllib.urlencode(qs)
+    r = requests.get(url)
+    d.update(r.json())
+    # Fill info
+    ids = '-'.join([str(x['id']) for x in d['result']['items']])
+    if ids:
+        qs = {
+            'hashes': ids,
+        }
+        url = API_URL + 'json_info?' + urllib.urlencode(qs)
+        r = requests.get(url)
+        j = r.json()
+
+        for x in d['result']['items']:
+            x.update(j[str(x['id'])])
+            x['magnet_url'] = 'magnet:?' + urllib.urlencode({'xt':'urn:btih:%s'% (x['info_hash']), 'dn':x['name'].encode('utf8')})
+            if 'files' in x:
+                x['files'] = [y for y in x['files'] if not y['path'].startswith(u'_')][:5]
+                x['files'].sort(key=lambda x:x['length'], reverse=True)
+            else:
+                x['files'] = [{'path': x['name'], 'length': x['length']}]
+    # pagination
+    w = 10
+    total = int(d['result']['meta']['total'])
+    d['page_max'] = total / d['ps'] if total % d['ps'] == 0 else total/d['ps'] + 1
+    d['prev_pages'] = range( max(d['p']-w+min(int(w/2), d['page_max']-d['p']),1), d['p'])
+    d['next_pages'] = range( d['p']+1, int(min(d['page_max']+1, max(d['p']-w/2,1) + w )) )
+    return render(request, 'list.html', d)
+
+
