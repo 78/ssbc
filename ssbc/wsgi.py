c5b248ab46895fd7fe72915e9a6e17740c8bd9f7
chienkuo chang <janegle@gmail.com>
2016-07-26 15:08:34 +0800

    os.environ to set var
    os.environ to set var r

--- ssbc/wsgi.py
+++ ssbc/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ssbc.settings")
+os.environ["DJANGO_SETTINGS_MODULE"] = "ssbc.settings"
 
 application = get_wsgi_application()
